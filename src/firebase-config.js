1312f6cdbd8384b9cb61f4b8d77d2a928a15dbeb
Jabu Kekana <95377670+JabuKekana@users.noreply.github.com>
2023-10-16 13:31:39 +0200

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,12 +5,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA6k8iNq3803ZYmicNayraGRncd_xJTC4g",
-  authDomain: "open-day-18e43.firebaseapp.com",
-  projectId: "open-day-18e43",
-  storageBucket: "open-day-18e43.appspot.com",
-  messagingSenderId: "1034275723641",
-  appId: "1:1034275723641:web:1f41ff256005a05b70f7a8"
+
 };
 
 // Initialize Firebase
